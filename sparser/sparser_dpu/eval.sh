0fbc916458a606856cf951966e25843ff5f1e95a
larry <yunpengliu199703@gmail.com>
2020-07-30 20:30:12 -0700

    slightly modified script

--- sparser/sparser_dpu/eval.sh
+++ sparser/sparser_dpu/eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# $1 filename $2 number of tasklets $3 number of DPUs
+# $1 filename $2 number of tasklets $3 number of DPUs $4 Query Key $5
 sed -i '4d' Makefile
 sed -i "4 i NR_TASKLETS\ ?=\ $2" Makefile
 sed -i '5d' Makefile
@@ -9,4 +9,6 @@ sed -i '6d' Makefile
 sed -i "6 i QUERY\ ?=\ $4" Makefile
 make clean
 make 
-./bench $1
+./bench $1 $5
+sleep 60
+./bench $1 $5
