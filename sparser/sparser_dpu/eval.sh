b0a4454683c7ff9b7ec16a92f3a97e8ab69f591e
larry <yunpengliu199703@gmail.com>
2020-08-06 15:06:07 -0700

    auto script testing

--- sparser/sparser_dpu/eval.sh
+++ sparser/sparser_dpu/eval.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# $1 filename $2 number of tasklets $3 number of DPUs
+# $1 filename $2 number of the queries $3 number of tasklets $4 number of DPUs $5 query name
 sed -i '4d' Makefile
-sed -i "4 i NR_TASKLETS\ ?=\ $2" Makefile
+sed -i "4 i NR_TASKLETS\ ?=\ $3" Makefile
 sed -i '5d' Makefile
-sed -i "5 i NR_DPUS\ ?=\ $3" Makefile
+sed -i "5 i NR_DPUS\ ?=\ $4" Makefile
 sed -i '6d' Makefile
-sed -i "6 i QUERY\ ?=\ $4" Makefile
+sed -i "6 i QUERY\ ?=\ $5" Makefile
 make clean
 make 
-./bench $1
+./bench $1 5
+sleep 45
+./bench $1 6
+sleep 45
+./bench $1 8
+sleep 45
+./bench $1 9
+sleep 45
+./bench $1 10
+sleep 45
+./bench $1 11
