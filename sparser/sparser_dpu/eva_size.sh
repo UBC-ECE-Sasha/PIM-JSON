2c52030eceb7f9e95ff4d4e699f5f92f575e79b0
larry <yunpengliu199703@gmail.com>
2020-08-18 13:34:34 -0700

    added more file size

--- sparser/sparser_dpu/eva_size.sh
+++ sparser/sparser_dpu/eva_size.sh
@@ -20,11 +20,6 @@ echo "#### 1024 start ####" >> eval_size.txt
 echo " " >> eval_size.txt
 echo " " >> eval_size.txt
 echo "1024 done"
-echo "#### 2048 start ####" >> eval_size.txt
-./eval_size_sub.sh ../../../tweet_2GB.json 2 16 512
-echo " " >> eval_size.txt
-echo " " >> eval_size.txt
-echo "2048 done"
 echo "#### 4096 start ####" >> eval_size.txt
 ./eval_size_sub.sh ../../../tweet_4GB.json 2 16 512
 echo " " >> eval_size.txt
@@ -35,11 +30,31 @@ echo "#### 8GB start ####" >> eval_size.txt
 echo " " >> eval_size.txt
 echo " " >> eval_size.txt
 echo "8GB done"
+echo "#### 12GB start ####" >> eval_size.txt
+./eval_size_sub.sh ../../../tweet_12GB.json 2 16 512
+echo " " >> eval_size.txt
+echo " " >> eval_size.txt
+echo "12GB done"
 echo "#### 16GB start ####" >> eval_size.txt
 ./eval_size_sub.sh ../../../tweet_16GB.json 2 16 512
 echo " " >> eval_size.txt
 echo " " >> eval_size.txt
 echo "16GB done"
+echo "#### 20GB start ####" >> eval_size.txt
+./eval_size_sub.sh ../../../tweet_20GB.json 2 16 512
+echo " " >> eval_size.txt
+echo " " >> eval_size.txt
+echo "20GB done"
+echo "#### 22GB start ####" >> eval_size.txt
+./eval_size_sub.sh ../../../tweet_22GB.json 2 16 512
+echo " " >> eval_size.txt
+echo " " >> eval_size.txt
+echo "22GB done"
+echo "#### 24GB start ####" >> eval_size.txt
+./eval_size_sub.sh ../../../tweet_22GB.json 2 16 512
+echo " " >> eval_size.txt
+echo " " >> eval_size.txt
+echo "24GB done"
 echo "#### 30GB start ####" >> eval_size.txt
 ./eval_size_sub.sh ../../../tweet_30GB.json 2 16 512
 echo " " >> eval_size.txt
