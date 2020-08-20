5b22a0600a5b2f0e6efe9661bbebfdd201608938
larry <yunpengliu199703@gmail.com>
2020-08-20 12:25:18 -0700

    add more dpu to the cost table

--- sparser/sparser_dpu/eval_COST_yelp.sh
+++ sparser/sparser_dpu/eval_COST_yelp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo " " > eval_cost_yelp.txt
 echo $1 >> eval_cost_yelp.txt
-for i in 64 110 120 125 130 140 145 160 180 320 384 448 512
+for i in 32 64 96 120 160 192 224 256 288 320 352 384 416 448 480 512
 do
   echo "#### $i start ####" >> eval_cost_yelp.txt
   ./eval.sh $1 2 16 $i eval_cost_yelp.txt
