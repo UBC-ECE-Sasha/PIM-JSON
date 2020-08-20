cd4e745ed6ef83e1242aafbfc31bdbb1bb522fa2
larry <yunpengliu199703@gmail.com>
2020-08-20 12:58:00 -0700

    minor fix

--- sparser/sparser_dpu/eval_COST_master.sh
+++ sparser/sparser_dpu/eval_COST_master.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo " " > eval_cost_yelp.txt
 echo "yelp_64MB.json start" >>  eval_cost_yelp.txt
-./eval_COST_yelp.sh yelp_64MB.json
+./eval_COST_yelp.sh ../../../yelp_64MB.json
 echo "-------------------------------" >> eval_cost_yelp.txt
 echo "yelp_256MB.json start" >>  eval_cost_yelp.txt
-./eval_COST_yelp.sh yelp_256MB.json
+./eval_COST_yelp.sh ../../../yelp_256MB.json
 echo "-------------------------------" >> eval_cost_yelp.txt
 echo "yelp_400MB.json start" >>  eval_cost_yelp.txt
-./eval_COST_yelp.sh yelp_400MB.json
+./eval_COST_yelp.sh ../../../yelp_400MB.json
 echo "-------------------------------" >> eval_cost_yelp.txt
 echo "yelp_640MB.json start" >>  eval_cost_yelp.txt
-./eval_COST_yelp.sh yelp_640MB.json
+./eval_COST_yelp.sh ../../../yelp_640MB.json
